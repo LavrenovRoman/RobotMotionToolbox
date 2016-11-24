4ffcb6f1e0e18cdb52246ec97d26c20c717affb3
LavrenovRoman <lavrenov.roman@gmail.com>
2016-11-24 16:41:23 +0300

    add test version of move control points

--- environment/rmt_iterations_with_creterias.m
+++ environment/rmt_iterations_with_creterias.m
@@ -1,15 +1,23 @@
 function [ paths ] = rmt_iterations_with_creterias (data, Nobstacles, X1, criterii_length, criterii_curve )
 
+    cRand = 10;
+    dlt = 0.1;
+
     for h=1:length(data.Homotopies)
         
-        Draw(data);
-        [cLen, cCurv] = rmt_calcCriterias(data.Vertex_Cord_DV, data.DVPaths{h,1}, Nobstacles, X1);
-        
+        nP = data.Homotopies(h, 1);
+        path = zeros(length(data.DVPaths{nP,1}), 2);
+        for i=1:length(data.DVPaths{nP,1})
+            path(i,1) = data.Vertex_Cord_DV(data.DVPaths{nP,1}(1,i),1);
+            path(i,2) = data.Vertex_Cord_DV(data.DVPaths{nP,1}(1,i),2);
+        end;
+        Draw(data, path);        
         changePoints = [];
+        changePointsDlt = [];
         pb=1;
-        for i=1:length(data.DVVerts{h,1})
-            for j=pb:length(data.DVPaths{h,1})
-                if data.DVPaths{h,1}(1,j) == data.DVVerts{h,1}(1,i)
+        for i=1:length(data.DVVerts{nP,1})
+            for j=pb:length(data.DVPaths{nP,1})
+                if data.DVPaths{nP,1}(1,j) == data.DVVerts{nP,1}(1,i)
                     changePoints = [changePoints j];
                     break;
                 end;
@@ -19,13 +27,41 @@ function [ paths ] = rmt_iterations_with_creterias (data, Nobstacles, X1, criter
         if 2 ~= changePoints(1,1)
             changePoints = [2 changePoints];
         end;
-        if length(data.DVPaths{h,1})-1 ~= changePoints(1,length(changePoints))
-            changePoints = [changePoints length(data.DVPaths{h,1})-1];
+        if length(data.DVPaths{nP,1})-1 ~= changePoints(1,length(changePoints))
+            changePoints = [changePoints length(data.DVPaths{nP,1})-1];
+        end;
+        
+        [cLen, cCurv] = rmt_calcCriterias(path, Nobstacles, X1);
+        tempPath = path;
+        for i=1:length(changePoints)
+            results = zeros(cRand, 2);
+            deltas  = zeros(cRand, 2);
+            resultsC = zeros(cRand, 2);
+            for j=1:cRand
+                xr = rand(1) - 0.5;
+                yr = rand(1) - 0.5;
+                x = dlt*xr/sqrt(xr^2 + yr^2);
+                y = dlt*yr/sqrt(xr^2 + yr^2);
+                deltas(j, 1) = x;
+                deltas(j, 2) = y;
+                path(changePoints(i), 1) = path(changePoints(i), 1) + x;
+                path(changePoints(i), 2) = path(changePoints(i), 2) + y;
+                [cLenN, cCurvN] = rmt_calcCriterias(path, Nobstacles, X1);
+                [bLenN, bCurvN] = checkCriterias(cLen, cCurv, cLenN, cCurvN);
+                results(j, 1) = bLenN;
+                results(j, 2) = bCurvN;
+                resultsC(j, 1) = cLenN;
+                resultsC(j, 2) = cCurvN;                
+                
+                tempPath(changePoints(i), 1) = path(changePoints(i), 1) - x;
+                tempPath(changePoints(i), 2) = path(changePoints(i), 2) - y;
+                
+                Draw(data, tempPath)
+            end;
         end;
         
         stopIteration = 0;
         while stopIteration == 0
-            
             
             stopIteration = 1;
         end;
@@ -33,20 +69,31 @@ function [ paths ] = rmt_iterations_with_creterias (data, Nobstacles, X1, criter
 
 end
 
-function [cLen, cCurv] = rmt_calcCriterias(Vertex_Cord_DV, path, Nobstacles, X1)
+function [bLen, bCurv] = checkCriterias(cLen, cCurv, cLenN, cCurvN)
+    bLen = 0;
+    bCurv = 0;
+    if cLen>cLenN
+        bLen = 1;
+    end;
+    if cCurv>=cCurvN
+        bCurv = 1;
+    end;
+end
+
+function [cLen, cCurv] = rmt_calcCriterias(path, Nobstacles, X1)
     % maxLength
     cLen = 0;
     for i=1:length(path)-1
-        x=[Vertex_Cord_DV(path(i),1) Vertex_Cord_DV(path(i+1),1)];
-        y=[Vertex_Cord_DV(path(i),2) Vertex_Cord_DV(path(i+1),2)];
+        x=[path(i,1) path(i+1,1)];
+        y=[path(i,2) path(i+1,2)];
         cLen = cLen + sqrt((x(1,2)-x(1,1))^2 + (y(1,2)-y(1,1))^2);
     end;
     
     % maxCurve
     cCurv = 180;
     for i=1:length(path)-2
-        x=[Vertex_Cord_DV(path(i),1) Vertex_Cord_DV(path(i+1),1) Vertex_Cord_DV(path(i+2),1)];
-        y=[Vertex_Cord_DV(path(i),2) Vertex_Cord_DV(path(i+1),2) Vertex_Cord_DV(path(i+2),2)];
+        x=[path(i,1) path(i+1,1) path(i+2,1)];
+        y=[path(i,2) path(i+1,2) path(i+2,2)];
         x1 = x(1,1)-x(1,2);
         x2 = x(1,3)-x(1,2);
         y1 = y(1,1)-y(1,2);
@@ -59,7 +106,7 @@ function [cLen, cCurv] = rmt_calcCriterias(Vertex_Cord_DV, path, Nobstacles, X1)
     end;
 end
 
-function [] = Draw(data)
+function [] = Draw(data, path)
     cla(data.handle_env);%new
     axes(data.handle_env);
     rmt_plot_environment(data.obstacles,data.frame_limits);                
@@ -70,7 +117,7 @@ function [] = Draw(data)
     end;
     for i=1:length(data.Edges(1,:))
         plot([data.Edges(1,i) data.Edges(2,i)],[data.Edges(3,i) data.Edges(4,i)],'color',[.8 .8 .8]);
-    end
+    end;
     for i=1:length(data.Verts(:,1))
         if i<5
             plot(data.Verts(i,1),data.Verts(i,2),'*','color','Green','LineWidth',2);
@@ -78,6 +125,13 @@ function [] = Draw(data)
             plot(data.Verts(i,1),data.Verts(i,2),'*','color','Red','LineWidth',2);
         end;
     end;
+    for i=1:length(path)-1
+        x=[path(i,1) path(i+1,1)];
+        y=[path(i,2) path(i+1,2)];
+        plot(x,y,'-','color','r','LineWidth',2);
+    end;
+    drawnow;
+    hold on;
 end
 
   
